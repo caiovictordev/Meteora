9e47bc8a355a0dab82ee7f333da5a56663eaa8bf
Winsantos <wingridnicoly19@gmail.com>
2026-05-28 10:29:48 -0300

    alteração no html, JavaScript concluído, e finalizei o responsivo

---
+++ script/module/app.js
@@ -0,0 +1,28 @@
+const emailInput = document.querySelector("emailInput")
+const resultMessage = document.querySelector("resultMessage")
+const emailForm = document.querySelector("emailForm") 
+
+// Um evento que controla/ativa após a submissão do form
+emailForm.addEventListener("submit", function (event){
+  event.preventDefault()
+
+  const email = emailInput.value
+
+  // Validar email
+  const isValid = validateEmail(email)
+
+  if(isValid){
+        resultMessage.textContent = "E-mail válido!"
+        resultMessage.style.color = "green"
+  }else{
+        resultMessage.textContent = "E-mail inválido!"
+        resultMessage.style.color = "red"
+  }
+})
+
+// Função de validar e-mail
+const validareEmail = (email) => {
+    // Padrão: TEXTO@TEXTO.TEXTO
+    const regex = /^[^\s]+@[^\s]+\.[^\s]+$/
+    return regex.test(email)
+} 
